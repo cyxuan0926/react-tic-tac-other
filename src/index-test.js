95e2f2745320b2f7f6fd5955ad431c954be4a407
cy0926 <840473997@qq.com>
2019-04-11 10:01:34 +0800

    first commit

---
+++ src/index-test.js
@@ -0,0 +1,162 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+// JSX <div/>:React.createElement('div') state
+// 在js class中，每次你定义其子类的构造函数时，都需要调用super方法，因此在所有含有
+// 构造函数的React组件中，构造函数必须以super(props)开头
+// 函数组件：只有一个render方法,并且不包含state 接收props作为参数，然后返回需要渲染的元素
+// 本质就是3x3的矩阵 点击后填充字符'X' 或者'O' 
+// 每一个网格
+function Square(props) {
+  return (
+    <button 
+      className="square"
+      onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+// class Square extends React.Component {
+//   render() {
+//     return (
+//       <button 
+//         className="square"
+//         onClick={() =>{ this.props.onClick() }}>
+//         {this.props.value}
+//       </button>
+//     );
+//   }
+// }
+// 整个矩阵
+class Board extends React.Component {
+  // handleClick(i) {
+  //   // state.squares数组的一个副本
+  //   // 一般来说，有两种改变数据的方式。第一种方式是直接修改变量的值
+  //   // 第二种是使用新的一份数据替换旧数据
+  //   // 好处：简化复杂的功能
+  //   const squares = this.state.squares.slice();
+  //   if(calculateWinner(squares) || squares[i]) return;
+  //   squares[i] = this.state.xIsNext ? 'X' :'O'; 
+  //   this.setState({
+  //     squares, 
+  //     xIsNext: !this.state.xIsNext
+  //   });
+  // }
+  renderSquare(i) {
+    return <Square 
+              value={this.props.squares[i]}
+              onClick={() => {this.props.onClick(i)}}/>;
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+// 整个布局
+class Game extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null)
+      }],
+      xIsNext: true,
+      stepNumber: 0, //当前正在查看哪一项历史纪录
+    }
+  }
+  handleClick(i) {
+    // 点击一下 要保存上一步的内容
+    // 行号 列号(2,3)
+    const history = this.state.history.slice(0, this.state.stepNumber+1); // 历史内容
+    const current = history[history.length - 1]; // 当前这一步
+    const squares = current.squares.slice(); // 当前这一步的数组
+    if(calculateWinner(squares) || squares[i]) return;
+    squares[i] = this.state.xIsNext ? 'X' : 'O'; 
+    this.setState({
+      history: history.concat([{squares}]), // 更新历史内容
+      xIsNext: !this.state.xIsNext, // 变更下一个出现的符号 
+      stepNumber: history.length, // 当前步数同步变化
+    })
+  }
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    })
+  }
+  render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+    const moves = history.map((step,move)=>{
+      const desc = move ? 'Go to move #' + move : 'Go to game start';
+      return (
+        <li key={move}>
+          <button onClick={()=>{this.jumpTo(move)}}>{desc}</button>
+        </li>
+      )
+    });
+    let status = winner ? 'Winner: ' + winner : 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board 
+            squares={current.squares}
+            onClick={(i)=> this.handleClick(i)}/>
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
+          <ol>{moves}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+// 判断出胜者：就是这几种情况下就会有胜者
+function calculateWinner(squares) {
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+  for(let i = 0;i<lines.length;i++) {
+    const [a,b,c]= lines[i]
+    if(squares[a] && squares[a]===squares[b] && squares[a]===squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+// 在游戏历史记录列表显示每一步棋的坐标，格式为 (列号, 行号):在不改变项目数据结构的情况下
+// function judgeCoordinates() {
+//   const values = []
+// } 
+// ========================================
+// react是一个声明式，高效且灵活的用于构建用户界面的js库
+// ReactDOM.render(
+//   <Game />,
+//   document.getElementById('root')
+// );
--- src/index.js
+++ src/index.js
@@ -1,12 +1,224 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+// 添加一个可以升序或降序显示历史纪录的按钮
+// 当无人获胜时 显示一个平局 先判断当前是否获胜 再判断下一步是否什么符号 然后再接下来的ji
+function Square(props) {
+  return(
+    <button
+      style={(props.winnerArr && props.winnerArr.includes(props.index)) ? {background: '#ffb7b7'} : {} } 
+      className="square"
+      onClick={() => props.onClick()}>
+      {props.value}
+    </button>
+  );
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square
+              key = {i}
+              index={i}
+              winnerArr={this.props.winnerArr} 
+              value={this.props.squares[i]}
+              onClick={()=>this.props.onClick(i)}/>;
+  }
+  // 一个是9个元素 3X3 的排列
+  render() {
+    let temp = [], finaly;
+    this.props.squares.map((value, index) => {
+      let arr = this.renderSquare(index);
+      if( index % 3 === 0) {
+        temp.push([]);
+      }
+      temp[parseInt(index/3)].push(arr);
+      return arr;
+    });
+    finaly = temp.map((value,index) => {
+      return (
+        <div key={index}>
+          {value}
+        </div>
+      );
+    });
+    return (
+      <div>
+        {finaly}
+      </div>
+    );
+  }
+}
+// 历史纪录：每走一步就保存当前步的数组信息，回到某一步就是取出当前步的数组
+class Game extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      history: [{
+          squares: Array(9).fill(null), // 步数的历史内容
+          coordinates: Array(2).fill(null) // 坐标历史
+      }],
+      stepNumber: 0, // 当前步数
+      xIsNext: true, // 下一步是'X'还是'O'
+      ascendingOrder: true
+    }
+  }
+  handleClick(i) {
+    // 这是点击小方格 需要改变的是历史数据数组 下一步出现的元素 步数
+    const history = this.state.history.slice(0, this.state.stepNumber+1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if(calculateWinner(squares) || squares[i]) return;
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    const coordinates = judgeCoordinates(i);
+    this.setState({
+      xIsNext: !this.state.xIsNext,
+      history: history.concat([{squares,coordinates}]),
+      stepNumber: history.length,
+    })
+  }
+  jumpTo(step) {
+    // 这是跳转到第几步 需要改变的就是第几步 判断下一步出现的元素是什么
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0
+    });
+  }
+  handleSort() {
+    if(this.state.history.length === 1) return
+    this.setState({
+      ascendingOrder: !this.state.ascendingOrder
+    });
+  }
+  render() {
+    // 升序 降序：历史数组 倒序过来
+    // 现在的步数 倒序过来后 需要改变不
+    // 渲染函数 当数据发生变化的时候 重新渲染改变的部分 当前就是stepNumber style={weightStyle}
+    // 几个重点关注得东西 第一个现在是第几步 下一步是什么东西,现在得历史纪录数组, 现在得历史坐标数组
+    let ascendingOrder = this.state.ascendingOrder;
+    const history = ascendingOrder ? this.state.history.slice() : (this.state.history.slice().reverse());
+    let tsNumber = ascendingOrder ? this.state.stepNumber : history.length - this.state.stepNumber - 1;
+    const weightStyle = {fontWeight:'bolder'};
+    const current = history[tsNumber];
+    const winnerArr = calculateWinner(current.squares);
+    let moves = history.map((value, index) => {
+      let ts = ascendingOrder ? index : history.length -index -1;
+      let desc = ts ? `Go to move # ${ts} (${value.coordinates[0]},${value.coordinates[1]})` : 'Go to game start'
+      return (
+        <li key={ts}>
+          <button 
+            onClick={() => this.jumpTo(ts)}
+            style={(history.length !== 1 && ts === this.state.stepNumber) ? weightStyle : {}}>
+              {desc}
+          </button>
+        </li>
+      );
+    });
+    let status = winnerArr ? (winnerArr.length ? `Winner: ${current.squares[winnerArr[0]]}` : '平局') : 'Next player: '+ (this.state.xIsNext ? 'X' : 'O');
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board
+            winnerArr={winnerArr} 
+            onClick={(i) => this.handleClick(i)}
+            squares={current.squares}/>
+        </div>
+        <div className="game-info">
+          <div>
+            {status}
+            <button 
+              style={{marginLeft: 10+'px'}}
+              onClick={() => this.handleSort()}>
+                {ascendingOrder ? '降序': '升序'}
+            </button>
+          </div>
+          <ol>{moves}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+function calculateWinner(squares) {
+  // 获胜情况下的数组 只要这些数组里面有两个不同的情况 那就会产生平局
+  // 提前判断 三个数组剩下的时候 也是可能平局的 
+  let tempResultArr = [];
+  for(let [tempIndex, tempValue] of squares.entries()) {
+    if(!tempValue) tempResultArr.push(tempIndex);
+  }
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],  
+    [0, 3, 6],  
+    [1, 4, 7],  
+    [2, 5, 8],  
+    [0, 4, 8],
+    [2, 4, 6] 
+  ]
+  for(let index in lines) {
+    const [a, b, c] = lines[index];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) return [a, b, c];
+  }
+  if(tempResultArr.length <= 3) {
+    let temp = 0;
+    for(let indexs in tempResultArr) {    
+      for(let index in lines) {
+        const [a, b, c] = lines[index];
+        if([a, b, c].includes(tempResultArr[indexs])) {
+          let copy = [a, b, c].slice();
+          copy.splice([a,b,c].indexOf(tempResultArr[indexs]),1);
+          if(squares[copy[0]] && squares[copy[0]] && squares[copy[0]]!== squares[copy[1]]) {
+           temp = 0;
+          } else {
+            temp = temp + 1;
+            break;
+          }
+        }
+      } 
+      if(temp) break;
+    }
+    if(!temp) {
+      return [];
+    }
+  }
+  return null;
+}
+function judgeCoordinates(index) {
+  let result = []
+  switch (index) {
+    case 0:
+      result = [0, 0];
+      break;
+    case 1:
+      result = [0, 1];
+      break;
+    case 2:
+      result = [0, 2];
+      break;
+    case 3:
+      result = [1, 0];
+      break;
+    case 4:
+      result = [1, 1];
+      break;
+    case 5:
+      result = [1, 2];
+      break;
+    case 6:
+      result = [2, 0];
+      break;
+    case 7:
+      result = [2, 1];
+      break;
+    case 8:
+      result = [2, 2];
+      break;      
+    default:
+      result = [];
+  }
+  return result;   
+}
+// ========================================
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
